62067cb0822a6a9d7ec081a2332b83c3c0d18f15
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2023-08-23 09:25:59 +0530

    Add files via upload

---
+++ positve negative.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main(){
+	int a;
+	printf("ENTER NUMBER A=");
+	scanf("%d",&a);
+	{
+	
+	if(a>=0)
+	
+	printf("A is positive number");
+	else  
+	
+	printf("A is negative number");
+	}
+	
+	
+}

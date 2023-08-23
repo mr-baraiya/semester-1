e3a4b2c1ed956ac2a38ab6cb89867cd0daf0dcbb
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2023-08-23 09:25:22 +0530

    Add files via upload

---
+++ odd or even.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+	int a;
+	printf("ENTER NUMBER");
+	scanf("%d",&a);
+	
+	{
+		if(a%2==0)
+		printf("A IS EVEN NUMBER");
+		else
+		printf("A IS ODD NUMBER");
+		
+	}
+	
+	
+}

509d6b3d3ae6a78295516297ecfd5ac9f3d5bfe4
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2023-08-23 17:09:44 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+	float h,b,area;
+	printf("enter the value of hieght =");
+	scanf("%f",&h);
+	printf("enter the value of base =");
+	scanf("%f",&b);
+	area=h*b*0.5;
+	printf("area of triangle =%f",area);
+}

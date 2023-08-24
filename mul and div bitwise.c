914b8422eb09a52e48827a8dfcbde18ce4404240
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2023-08-24 12:29:38 +0530

    Add files via upload

---
+++ mul and div bitwise.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main(){
+	int a,mul,div;
+	printf("enter value of a=");
+	scanf("%d",&a);
+	mul=a<<1;
+	div=a>>1;
+	printf("multiplied answer is :%d    ",mul);
+	printf("divition answer is : %d",div);
+}

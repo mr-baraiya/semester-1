be0e7545e2a11bf8829dd206263ad2035df43bde
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2023-10-05 12:32:00 +0530

    Add files via upload

---
+++ 13-B-2 (A).c
@@ -0,0 +1,18 @@
+//1
+//2 3
+//4 5 6
+//7 8 9 10
+//11 12 13 14 15
+#include<stdio.h>
+void main(){
+	int i,j,n,k=1;
+	printf("enter the number =");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=i;j++){
+			printf("%3d",k);
+			k++;
+		}
+		printf("\n\a");
+	}
+}

0a7d64827c1a16ddbaa3e7e062339c11ddb0249b
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2023-10-12 19:36:58 +0530

    Add files via upload

---
+++ 15-B-3.c
@@ -0,0 +1,22 @@
+//. Swap first element with last, second to second last and so on.
+#include<stdio.h>
+
+void main(){
+	int n;
+	printf("Enter the size of array :-");
+	scanf("%d",&n);
+	int arr[n];
+	int i,j,temp=0;
+	for(i=0;i<n;i++){
+		printf("enter the number in arr[%d] = ",i);
+		scanf("%d",&arr[i]);
+	}
+	for(i=0;i<n/2;i++){
+		temp=arr[i];
+		arr[i]=arr[n-i-1];
+		arr[n-i-1]=temp;
+	}
+	for(i=0;i<n;i++){
+		printf("%d\n",arr[i]);
+	}
+}

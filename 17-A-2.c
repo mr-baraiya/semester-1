57345b8ce9854e66c02f0f2ee65050f1976ebd4a
baraiya79 <142961593+baraiya79@users.noreply.github.com>
2023-10-25 09:06:26 +0530

    Update 17-A-2.c

--- 17-A-2.c
+++ 17-A-2.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main(){
-	char a='A' , *ptr1;
+	char a="vishal", *ptr1;
 	int b= 10 , *ptr2;
 	float c=3.6789 , *ptr3;
 	double d=234.098 , *ptr4;
@@ -9,7 +9,7 @@ void main(){
 	ptr3=&c;
 	ptr4=&d;
 	
-	printf("%c \n",*ptr1);
+	printf("%s \n",ptr1);
 	printf("%d \n",*ptr2);
 	printf("%f \n",*ptr3);
 	printf("%lf \n",*ptr4);
